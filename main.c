57ada8b7b30a5c15b75361a6010297c40d360438
kapouchima <s.baghi@gmail.com>
2014-12-06 15:33:48 +0330

    Project Start

---
+++ main.c
@@ -0,0 +1,232 @@
+/*
+ * TubularJackController.c
+ *
+ * Created: 9/13/2014 9:58:26 AM
+ *  Author: baghi
+ */ 
+
+#define F_CPU 10000000UL  // 10 MHz
+#define Open 1
+#define Close 0
+#define ActionTime (38*30)
+#define FlasherDelay (38*1)
+#define AutocloseDelay (38*60)
+
+
+#include <avr/io.h>
+#include <avr/delay.h>
+#include "BoardAbstraction.h"
+
+
+
+
+char RemoteAFlag=0,RemoteBFlag=0,State=0,FlasherFlag=0,FlasherState=0;
+unsigned long long Time=0,MotorStartTime,MotorStopTime;
+
+
+
+
+void StopMotor(void)
+{
+	Relay1Reset;Relay2Reset;
+	FlasherFlag=0;	
+}
+
+
+
+
+
+
+void StartMotor(char dir)
+{
+	FlasherFlag=1;
+	if(dir==Open)
+	{
+		Relay2Reset;Relay1Set;
+	}
+	
+	if(dir==Close)
+	{
+		Relay1Reset;Relay2Set;
+	}
+	
+}
+
+
+
+
+
+
+
+void State0(void) // closed
+{
+	if((RemoteAFlag)||(!OpenKey))
+	{
+		RemoteAFlag=0;
+		LEDSet;
+		MotorStartTime=Time;
+		StartMotor(Open);
+		State=1;
+		_delay_ms(500);
+	}
+}
+
+
+
+
+
+
+void State1(void) //openning
+{
+	if((RemoteAFlag)||(!StopKey)||(!CloseKey))
+	{
+		RemoteAFlag=0;
+		MotorStopTime=Time;
+		StopMotor();
+		State=2;
+		_delay_ms(500);
+	}
+	
+	if(Time>(MotorStartTime+ActionTime))
+	{
+		MotorStopTime=Time;
+		StopMotor();
+		State=2;
+	}
+	
+	
+}
+
+
+
+
+
+
+void State2(void) //open
+{
+	if((RemoteAFlag)||(!CloseKey))
+	{
+		RemoteAFlag=0;
+		MotorStartTime=Time;
+		StartMotor(Close);
+		State=3;
+		_delay_ms(500);
+	}
+	
+	if((MotorStopTime!=0)&&(Key2))
+	{
+		if(Time>(MotorStopTime+AutocloseDelay))
+		{
+			MotorStartTime=Time;
+			StartMotor(Close);
+			State=3;
+			_delay_ms(500);
+		}
+	}	
+}
+
+
+
+
+
+void State3(void) //closing
+{
+	if((RemoteAFlag)||(!OpenKey)||(!StopKey))
+	{
+		RemoteAFlag=0;
+		MotorStopTime=Time;
+		StopMotor();
+		State=2;
+		_delay_ms(500);
+	}
+	
+	if(IRSensor)
+	{
+		StopMotor();
+		_delay_ms(500);
+		MotorStartTime=Time;
+		StartMotor(Open);
+		_delay_ms(500);
+		State=1;
+	}
+	
+	if(Time>(MotorStartTime+ActionTime))
+	{
+		LEDReset;
+		MotorStopTime=0;
+		StopMotor();
+		State=0;
+	}
+	
+}
+
+
+
+
+
+
+void StateManager(void)
+{
+  if(State==0)
+    State0();
+  
+  if(State==1)
+	State1();
+  
+  if(State==2)
+	State2();
+  
+  if(State==3)
+	State3();	 	
+}
+
+
+
+
+
+
+void Flasher(void)
+{
+	if(FlasherFlag)
+	{
+			if(FlasherState==0)
+				{Relay3Reset;}		
+			else
+				{Relay3Set;}
+	}
+	else
+	{
+		Relay3Reset;FlasherState=0;
+	}		
+	  
+}
+
+
+
+
+
+
+int main(void)
+{
+	
+	static char state=0;
+    
+	Startup();
+	LEDReset;
+	
+	/*
+	while(1)
+	if(Key2)
+	  LEDSet;
+	  else
+	  LEDReset;
+	*/
+	
+	
+	
+	while(1)
+    {
+		Flasher(); 
+		StateManager();		
+    }
+}
